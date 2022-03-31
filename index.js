a60d791bf816191b125c0d1764e8688bc8955b22
jim <jimdevelopesite@gmail.com>
2022-03-31 14:42:59 +0800

    update static path

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session({
