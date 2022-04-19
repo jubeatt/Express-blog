0edb20ece3c2a8a802aa7aafdcf51c5f4926705f
jim <jimdevelopesite@gmail.com>
2022-04-19 17:20:58 +0800

    fix highlight style bug

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
-{
+require("dotenv").config();
+
+module.exports = {
   "development": {
     "username": "root",
     "password": "root",
     "database": "database_development",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "use_env_variable": "DATABASE_URL"
   },
   "test": {
     "username": "root",
